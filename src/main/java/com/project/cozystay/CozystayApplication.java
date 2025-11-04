f64923dbe632fa0858c91ca605f8695d10c52380
mojiri00 <moguricamera@gmail.com>
2025-11-04 17:22:59 +0900

    Feat: CozyStay BackEnd 스프링부트 초기 설정

---
+++ src/main/java/com/project/cozystay/CozystayApplication.java
@@ -0,0 +1,13 @@
+package com.project.cozystay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozystayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozystayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cozystay/CozystayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cozystay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozystayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
