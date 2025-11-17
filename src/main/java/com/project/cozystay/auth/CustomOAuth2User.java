d057f16540b014181cc02ceb1aea53b104d381aa
mojiri00 <moguricamera@gmail.com>
2025-11-17 12:45:43 +0900

    feat(CustomOAuth2User): CustomOAuth2User 구현

---
+++ src/main/java/com/project/cozystay/auth/CustomOAuth2User.java
@@ -0,0 +1,46 @@
+package com.project.cozystay.auth;
+
+import com.project.cozystay.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User {
+
+    private final User user;
+    private final Map<String, Object> attributes;
+
+    public CustomOAuth2User(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    public String getEmail() {
+        return user.getEmail();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // 간단히 ROLE_USER 하나만 준다고 가정
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getName() {
+        // Security가 내부에서 사용하는 "이름"
+        return user.getEmail(); // 또는 member.getId().toString()
+    }
+}
