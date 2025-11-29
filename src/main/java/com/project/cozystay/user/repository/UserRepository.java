d5b4edd1fd65fb0e15e567347d48d0389a33bc2d
mojiri00 <moguricamera@gmail.com>
2025-11-29 23:21:14 +0900

    feat(UserRepository): User 레포지토리 구현

--- src/main/java/com/project/cozystay/user/repository/UserRepository.java
+++ src/main/java/com/project/cozystay/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.project.cozystay.user.repository;
 
+import com.project.cozystay.user.domain.AuthProvider;
 import com.project.cozystay.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,12 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // 카카오 ID로 회원 조회
-    Optional<User> findByKakaoId(Long kakaoId);
+    Optional<User> findByProviderId(String providerId);
+
+    // DB PK ID로 회원 조회
+    Optional<User> findUserById(Long userId);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByProviderAndProviderId(AuthProvider provider, String providerId);
 }
