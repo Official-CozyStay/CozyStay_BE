b9e87942e1750bedb4665c6dd2265fe42c5c7e1c
mojiri00 <moguricamera@gmail.com>
2025-12-09 01:05:56 +0900

    fix(UserReviewService): userReviewRepository -> userRepository로 변경

--- src/main/java/com/project/cozystay/review/service/UserReviewService.java
+++ src/main/java/com/project/cozystay/review/service/UserReviewService.java
@@ -48,7 +48,7 @@ public class UserReviewService {
     // 사용자 리뷰 조회
     public List<UserReviewResponse> getUserReviews(Long targetUserId){
 
-        if (!userReviewRepository.existsById(targetUserId)) {
+        if (!userRepository.existsById(targetUserId)) {
             throw new IllegalArgumentException("존재하지 않는 사용자입니다. id=" + targetUserId);
         }
 
