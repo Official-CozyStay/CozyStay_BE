f0f8eeafcae5fb0afdbe9f34b071481d1f4e1469
mojiri00 <moguricamera@gmail.com>
2025-12-09 13:59:22 +0900

    fix(AccommodationReviewResponse): 빌더 삭제

--- src/main/java/com/project/cozystay/review/dto/AccommodationReviewResponse.java
+++ src/main/java/com/project/cozystay/review/dto/AccommodationReviewResponse.java
@@ -9,7 +9,6 @@ import java.math.BigDecimal;
  * 게스트/사용자 -> 숙소
  * 리뷰 응답 DTO
  */
-@Builder
 public record AccommodationReviewResponse (
 
     // 숙소 ID
@@ -29,15 +28,15 @@ public record AccommodationReviewResponse (
     // Entity -> DTO
     public static AccommodationReviewResponse from(AccommodationReview accommodationReview) {
 
-        return AccommodationReviewResponse.builder()
-                .id(accommodationReview.getId())
-                .ratingOverall(accommodationReview.getRatingOverall())
-                .ratingCleanliness(accommodationReview.getRatingCleanliness())
-                .ratingAccuracy(accommodationReview.getRatingAccuracy())
-                .ratingCheckin(accommodationReview.getRatingCheckin())
-                .ratingCommunication(accommodationReview.getRatingCommunication())
-                .ratingLocation(accommodationReview.getRatingLocation())
-                .comment(accommodationReview.getComment())
-                .build();
+        return new AccommodationReviewResponse(
+                accommodationReview.getId(),
+                accommodationReview.getRatingOverall(),
+                accommodationReview.getRatingCleanliness(),
+                accommodationReview.getRatingAccuracy(),
+                accommodationReview.getRatingCheckin(),
+                accommodationReview.getRatingCommunication(),
+                accommodationReview.getRatingLocation(),
+                accommodationReview.getComment()
+        );
     }
 }
