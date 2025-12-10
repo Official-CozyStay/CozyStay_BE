4a64f0f40444208dfeb5b352fb82a12597bee8ff
Youngjae so <terranbang@gmail.com>
2025-12-10 20:08:22 +0900

    feat(AccommodationDetail): update 메서드 구현 및 필드명 id로 변경

--- src/main/java/com/project/cozystay/accommodation/domain/AccommodationDetail.java
+++ src/main/java/com/project/cozystay/accommodation/domain/AccommodationDetail.java
@@ -1,6 +1,7 @@
 package com.project.cozystay.accommodation.domain;
 
 
+import com.project.cozystay.accommodation.dto.AccommodationDetailUpdateRequestDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -15,7 +16,7 @@ import lombok.*;
 public class AccommodationDetail {
     @Id
     @Column(name = "accommodation_id")
-    private Long accommodationId;
+    private Long id;
 
     @MapsId
     @OneToOne(fetch = FetchType.LAZY)
@@ -66,5 +67,36 @@ public class AccommodationDetail {
 
     void assignAccommodation(Accommodation accommodation){
         this.accommodation = accommodation;
+    }
+
+    public void update(AccommodationDetailUpdateRequestDTO dto) {
+
+        if (dto.getRoomCount() != null) this.roomCount = dto.getRoomCount();
+
+        if (dto.getBedroomCount() != null) this.bedroomCount = dto.getBedroomCount();
+
+        if (dto.getBedCount() != null) this.bedCount = dto.getBedCount();
+
+        if (dto.getBathroomCount() != null) this.bathroomCount = dto.getBathroomCount();
+
+        if (dto.getAirConditionerCount() != null) this.airConditionerCount = dto.getAirConditionerCount();
+
+        if (dto.getHairDryerCount() != null) this.hairDryerCount = dto.getHairDryerCount();
+
+        if (dto.getRefrigeratorCount() != null) this.refrigeratorCount = dto.getRefrigeratorCount();
+
+        if (dto.getTelevisionCount() != null) this.televisionCount = dto.getTelevisionCount();
+
+        if (dto.getWasherCount() != null) this.washerCount = dto.getWasherCount();
+
+        if (dto.getDryerCount() != null) this.dryerCount = dto.getDryerCount();
+
+        if (dto.getWifiAvailable() != null) this.wifiAvailable = dto.getWifiAvailable();
+
+        if (dto.getParkingAvailable() != null) this.parkingAvailable = dto.getParkingAvailable();
+
+        if (dto.getPetAvailable() != null) this.petAvailable = dto.getPetAvailable();
+
+        if (dto.getKitchenAvailable() != null) this.kitchenAvailable = dto.getKitchenAvailable();
     }
 }
