c14d5879e4cee5344d983ff13d87a8877bb0b50b
yeainlee <lyy923923@gmail.com>
2026-01-05 15:24:57 +0900

    refactor: 오타 수정

--- src/main/java/com/project/cozystay/booking/exception/InvalidDateRangeException.java
+++ src/main/java/com/project/cozystay/booking/exception/InvalidDateRangeException.java
@@ -11,7 +11,7 @@ public class InvalidDateRangeException extends RuntimeException{
 
     public static InvalidDateRangeException of(LocalDate from, LocalDate to){
         return new InvalidDateRangeException(
-                String.format("잘못된 날짜 범위입니다. from=%s, to=%s (from은 to보다 이전이어야 합니다.", from, to)
+                String.format("잘못된 날짜 범위입니다. from=%s, to=%s (from은 to보다 이전이어야 합니다.)", from, to)
         );
     }
 
