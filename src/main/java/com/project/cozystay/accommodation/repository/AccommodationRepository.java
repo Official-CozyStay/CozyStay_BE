78fd3728488ea4256d3cdd1c58b44224b72dc64e
Youngjae so <terranbang@gmail.com>
2026-01-19 21:17:57 +0900

    feat : 메인 페이지에서 활성화 된 숙소만 반환

--- src/main/java/com/project/cozystay/accommodation/repository/AccommodationRepository.java
+++ src/main/java/com/project/cozystay/accommodation/repository/AccommodationRepository.java
@@ -15,6 +15,7 @@ public interface AccommodationRepository extends JpaRepository<Accommodation, Lo
     SELECT DISTINCT a
     FROM Accommodation a
     LEFT JOIN FETCH a.images
+    WHERE a.status = 'ACTIVE'
 """)
     List<Accommodation> findAllAccommodations();
 
