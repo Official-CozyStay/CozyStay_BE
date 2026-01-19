1ee7984ede57d049533a50e166fb9f97c9a7ad43
Youngjae so <terranbang@gmail.com>
2026-01-19 14:08:43 +0900

    refactor : getFavoriteDetail 메서드 N+1 문제 해결 및 fetch join 메서드 추가

--- src/main/java/com/project/cozystay/favorite/repository/FavoriteRepository.java
+++ src/main/java/com/project/cozystay/favorite/repository/FavoriteRepository.java
@@ -2,6 +2,8 @@ package com.project.cozystay.favorite.repository;
 
 import com.project.cozystay.favorite.domain.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +13,16 @@ public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     List<Favorite> findAllByUser_Id(Long userId);
 
     Optional<Favorite> findByIdAndUser_Id(Long favoriteId, Long userId);
+
+    @Query("""
+            SELECT DISTINCT f
+            FROM Favorite f
+            LEFT JOIN FETCH f.favoriteAccommodations fa
+            LEFT JOIN FETCH fa.accommodation a
+            WHERE f.id = :favoriteId AND f.user.id = :userId
+    """)
+    Optional<Favorite> findDetailByIdAndUserId(
+            @Param("favoriteId")Long favoriteId,
+            @Param("userId") Long userId
+    );
 }
