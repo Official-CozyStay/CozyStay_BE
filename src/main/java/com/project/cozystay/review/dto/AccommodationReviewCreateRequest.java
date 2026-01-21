6ad616bd0334b401891b1a9113bc23085aba70f1
jmmo0722 <moguricamera@gmail.com>
2026-01-21 20:22:32 +0900

    refactor: 예약 Id 필드 추가 및 리뷰 필드명 변경

--- src/main/java/com/project/cozystay/review/dto/AccommodationReviewCreateRequest.java
+++ src/main/java/com/project/cozystay/review/dto/AccommodationReviewCreateRequest.java
@@ -1,6 +1,8 @@
 package com.project.cozystay.review.dto;
 
 import com.project.cozystay.accommodation.domain.Accommodation;
+import com.project.cozystay.booking.domain.Booking;
+import com.project.cozystay.comment.domain.Comment;
 import com.project.cozystay.review.domain.AccommodationReview;
 import com.project.cozystay.user.domain.User;
 
@@ -14,6 +16,7 @@ public record AccommodationReviewCreateRequest(
 
     // 숙소 ID
     Long accommodationId,
+    Long bookingId,
 
     // 평점
     BigDecimal ratingCleanliness,
@@ -23,7 +26,7 @@ public record AccommodationReviewCreateRequest(
     BigDecimal ratingLocation,
 
     // 리뷰
-    String comment
+    String reviewComment
 ) {
 
     // 기본 생성자
@@ -36,21 +39,20 @@ public record AccommodationReviewCreateRequest(
         ratingLocation = (ratingLocation == null) ? BigDecimal.ZERO : ratingLocation;
 
         // comment가 null이면 빈 문자열
-        comment = (comment == null) ? "" : comment;
+        reviewComment = (reviewComment == null) ? "" : reviewComment;
     }
 
     // DTO -> Entity
-    public AccommodationReview toEntity(Accommodation accommodation, User guest, BigDecimal ratingOverall) {
+    public AccommodationReview toEntity(Booking booking,
+                                        Accommodation accommodation,
+                                        User guest,
+                                        BigDecimal ratingOverall) {
         return AccommodationReview.of(
+                booking,
                 accommodation,
                 guest,
-                ratingOverall,
-                this.ratingCleanliness,
-                this.ratingAccuracy,
-                this.ratingCheckin,
-                this.ratingCommunication,
-                this.ratingLocation,
-                this.comment
+                this,
+                ratingOverall
         );
     }
 }
