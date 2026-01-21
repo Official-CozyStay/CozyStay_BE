8a3fb05c2110b1ae171333d95d7792ba72376930
jmmo0722 <moguricamera@gmail.com>
2026-01-21 20:25:44 +0900

    refactor: Comment, Booking에 따른 비즈니스 로직 추가 - bookingId를 통한 리뷰 중복 생성 방지 / - 댓글 여부에 따른 리뷰 수정 권한 로직 추가

--- src/main/java/com/project/cozystay/review/domain/UserReview.java
+++ src/main/java/com/project/cozystay/review/domain/UserReview.java
@@ -1,5 +1,7 @@
 package com.project.cozystay.review.domain;
 
+import com.project.cozystay.booking.domain.Booking;
+import com.project.cozystay.comment.domain.Comment;
 import com.project.cozystay.common.BaseTimeEntity;
 import com.project.cozystay.review.dto.UserReviewCreateRequest;
 import com.project.cozystay.user.domain.User;
@@ -15,7 +17,7 @@ import java.math.BigDecimal;
 @Getter
 @Table(name = "user_review")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserReview extends BaseTimeEntity {
+public class UserReview extends BaseTimeEntity implements Commentable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,42 +33,49 @@ public class UserReview extends BaseTimeEntity {
     private User targetGuest;
 
 
-//    @OneToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "booking_id", unique = true, nullable = false)
-//    private Booking booking;
-//
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booking_id", unique = true, nullable = false)
+    private Booking booking;
+
     @Column(name = "rating", nullable = false, precision = 2, scale = 1)
     private BigDecimal rating;
 
-    @Column(name = "comment", columnDefinition = "TEXT")
-    private String comment;
+    @Column(name = "review_comment", columnDefinition = "TEXT")
+    private String reviewComment;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
 
     // ==== 생성자 ====
     @Builder(access = AccessLevel.PRIVATE)
-    public UserReview(User reviewerHost, User targetGuest,
-                      //Booking booking,
-                      BigDecimal rating, String comment) {
+    public UserReview(Booking booking, User reviewerHost, User targetGuest,
+                      BigDecimal rating, String reviewComment) {
+        this.booking = booking;
         this.reviewerHost = reviewerHost;
         this.targetGuest = targetGuest;
-        //this.booking = booking;
         this.rating = rating;
-        this.comment = comment;
+        this.reviewComment = reviewComment;
     }
 
     // ==== 정적 팩토리 메서드 ====
-    public static UserReview of(User reviewerHost, User targetUser,
-                                //Booking booking,
-                                BigDecimal rating, String comment) {
+    public static UserReview of(Booking booking, User reviewerHost, User targetUser,
+                                BigDecimal rating, String reviewComment) {
 
         return UserReview.builder()
+                .booking(booking)
                 .reviewerHost(reviewerHost).targetGuest(targetUser)
-                .rating(rating).comment(comment)
+                .rating(rating).reviewComment(reviewComment)
                 .build();
     }
 
     // ==== 비즈니스 메서드 ====
     public void update(UserReviewCreateRequest request){
         this.rating = request.rating();
-        this.comment = request.comment();
+        this.reviewComment = request.reviewComment();
+    }
+
+    public void addComment(Comment comment) {
+        this.comment = comment;
     }
 }
