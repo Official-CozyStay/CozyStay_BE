e52d965b246e0b97b5d6b412f34008040423a8f5
mojiri00 <moguricamera@gmail.com>
2026-01-31 19:31:56 +0900

    fix: AccessDeniedException를 올바른 라이브러리를 import 할 수 있도록 변경

--- src/main/java/com/project/cozystay/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/cozystay/global/exception/GlobalExceptionHandler.java
@@ -3,11 +3,15 @@ package com.project.cozystay.global.exception;
 import com.project.cozystay.booking.exception.*;
 import com.project.cozystay.booking.guest.exception.*;
 import com.project.cozystay.comment.exception.CommentNotFoundException;
+import com.project.cozystay.booking.guest.exception.*;
+import com.project.cozystay.comment.exception.CommentNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -45,7 +49,7 @@ public class GlobalExceptionHandler {
 
     /* ===================== 403 FORBIDDEN ===================== */
     @ExceptionHandler(java.nio.file.AccessDeniedException.class)
-    public ResponseEntity<ErrorResponse> handleAccessDenied(java.nio.file.AccessDeniedException e) {
+    public ResponseEntity<ErrorResponse> handleAccessDenied(AccessDeniedException e) {
         log.warn("Forbidden [{}]: {}", e.getClass().getSimpleName(), e.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
                 .body(new ErrorResponse("접근 권한이 없습니다."));
