1d9b6801921a5a7d026fbd465953ef832559ff92
Youngjae so <terranbang@gmail.com>
2026-02-12 00:53:40 +0900

    feat : STOMP ChannelInterceptor 구현

---
+++ src/main/java/com/project/cozystay/config/StompHandler.java
@@ -0,0 +1,51 @@
+package com.project.cozystay.config;
+
+
+import com.project.cozystay.auth.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StompHandler implements ChannelInterceptor {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            String token = accessor.getFirstNativeHeader("Authorization");
+
+            if (token != null && token.startsWith("Bearer ")) {
+                token = token.substring(7);
+
+                if (jwtProvider.validateToken(token)) {
+                    Long userId = jwtProvider.getUserId(token);
+                    Authentication auth = new UsernamePasswordAuthenticationToken(
+                            userId, // Principal로 사용될 값 (컨트롤러에서 추출)
+                            null,
+                            Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+                    );
+                    accessor.setUser(auth);
+                    log.info("WebSocket 인증 성공 - UserId: {}", userId);
+                }
+            }
+        }
+        return message;
+    }
+}
