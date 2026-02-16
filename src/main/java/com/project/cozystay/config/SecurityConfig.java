d8176803a177a4b71a49ebde2ae82097b4059993
jmmo0722 <moguricamera@gmail.com>
2026-02-16 16:44:45 +0900

    refactor: 불필요한 import문 삭제

--- src/main/java/com/project/cozystay/config/SecurityConfig.java
+++ src/main/java/com/project/cozystay/config/SecurityConfig.java
@@ -23,16 +23,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpMethod;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-// 수정한 부분
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
