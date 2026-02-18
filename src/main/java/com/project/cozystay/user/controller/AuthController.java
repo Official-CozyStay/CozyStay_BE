b19117110c178fedcfec4f1098eed230a6e0fde3
jmmo0722 <moguricamera@gmail.com>
2026-02-18 22:02:38 +0900

    feat: 이메일 인증 요청 API, 이메일 인증 확인 API 추가

--- src/main/java/com/project/cozystay/user/controller/AuthController.java
+++ src/main/java/com/project/cozystay/user/controller/AuthController.java
@@ -1,24 +1,22 @@
 package com.project.cozystay.user.controller;
 
+import com.project.cozystay.auth.CustomOAuth2User;
 import com.project.cozystay.auth.CustomUserDetails;
 import com.project.cozystay.auth.JwtProvider;
-import com.project.cozystay.user.dto.ApiResponse;
+import com.project.cozystay.user.dto.*;
 import com.project.cozystay.user.domain.User;
-import com.project.cozystay.user.dto.SignInRequest;
-import com.project.cozystay.user.dto.SignInResponse;
-import com.project.cozystay.user.dto.SignUpRequest;
+import com.project.cozystay.user.service.EmailService;
 import com.project.cozystay.user.service.UserService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -26,39 +24,61 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final EmailService emailService;
+
     private final AuthenticationManager authenticationManager;
     private final JwtProvider jwtProvider;
 
+    /**
+     * 회원가입
+     */
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<Void>> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
+
+        if(emailService.isVerified(signUpRequest.email())){
+            ResponseEntity.badRequest().body("이메일 인증이 필요합니다");
+        }
+
         userService.signUp(signUpRequest);
         return ResponseEntity.ok(ApiResponse.success("회원가입이 완료되었습니다.", null));
     }
 
+    /**
+     * 로그인
+     */
     @PostMapping("/signin")
     public ResponseEntity<SignInResponse> signIn(@Valid @RequestBody SignInRequest signInRequest) {
 
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(signInRequest.username(), signInRequest.password())
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-        User user = userDetails.getUser();
-
-        String accessToken = jwtProvider.createAccessToken(user.getId(), user.getUserRole());
-        String refreshToken = jwtProvider.createRefreshToken(user.getId());
-
-        SignInResponse signInResponse = SignInResponse.builder()
-                .userId(user.getId())
-                .nickName(user.getNickName())
-                .role(user.getUserRole().name())
-                .grantType("Bearer")
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
+        SignInResponse signInResponse = userService.signIn(signInRequest);
 
         return ResponseEntity.ok(signInResponse);
+    }
+
+    /**
+     * 이메일 인증 요청
+     */
+    @PostMapping("/email-verification/request")
+    public ResponseEntity<String> requestEmailVerification(
+            @RequestParam @Email String email
+    ) {
+
+        emailService.sendVerificationEmail(email);
+
+        return ResponseEntity.ok("인증 코드를 보냈습니다! 📧");
+    }
+
+    // 인증 확인 (코드 검사)
+    @PostMapping("/verify")
+    public ResponseEntity<String> verifyEmail(
+            @RequestBody VerificationEmailRequestDTO request
+    ) {
+
+        boolean isVerified = emailService.verifyCode(request.email(), request.code());
+
+        if (isVerified) {
+            return ResponseEntity.ok("인증 성공! 환영합니다. 🎉");
+        } else {
+            return ResponseEntity.badRequest().body("인증 실패... 코드를 다시 확인해주세요.");
+        }
     }
 }
