0476b0166320524e570d19bf2c1afaee914ecb10
yeainlee <lyy923923@gmail.com>
2026-02-24 01:19:34 +0900

    feat: 호스트 거절/예약 취소 시 자동 환불 연결

--- src/main/java/com/project/cozystay/booking/service/BookingHostDecisionCommandService.java
+++ src/main/java/com/project/cozystay/booking/service/BookingHostDecisionCommandService.java
@@ -3,6 +3,7 @@ package com.project.cozystay.booking.service;
 import com.project.cozystay.booking.domain.Booking;
 import com.project.cozystay.booking.exception.BookingNotFoundException;
 import com.project.cozystay.booking.repository.BookingRepository;
+import com.project.cozystay.payment.service.PaymentCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class BookingHostDecisionCommandService {
 
     private final BookingRepository bookingRepository;
+    private final PaymentCommandService paymentCommandService;
 
     @Transactional
     public void confirm(Long bookingId, Long hostId) {
@@ -27,5 +29,8 @@ public class BookingHostDecisionCommandService {
                 .orElseThrow(()->new BookingNotFoundException(bookingId));
 
         booking.rejectByHost();
+
+        // 호스트 거절 시 자동 환불
+        paymentCommandService.refundByBooking(bookingId);
     }
 }
