3f5000acb1d967bea8326886e0e743c259382692
jmmo0722 <moguricamera@gmail.com>
2026-02-26 19:58:44 +0900

    refactor: 불필요한 import 제거

--- src/main/java/com/project/cozystay/user/controller/AuthController.java
+++ src/main/java/com/project/cozystay/user/controller/AuthController.java
@@ -1,22 +1,14 @@
 package com.project.cozystay.user.controller;
 
-import com.project.cozystay.auth.CustomOAuth2User;
-import com.project.cozystay.auth.CustomUserDetails;
 import com.project.cozystay.auth.JwtProvider;
 import com.project.cozystay.user.dto.*;
-import com.project.cozystay.user.domain.User;
 import com.project.cozystay.user.service.EmailService;
 import com.project.cozystay.user.service.UserService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
