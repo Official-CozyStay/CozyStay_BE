289e28695773133cfbcceed673d8d44c49278ba9
jmmo0722 <moguricamera@gmail.com>
2026-03-03 17:20:05 +0900

    refactor: findByAccommodation_Id() User 패치 조인 사용

--- src/main/java/com/project/cozystay/review/repository/AccommodationReviewRepository.java
+++ src/main/java/com/project/cozystay/review/repository/AccommodationReviewRepository.java
@@ -10,7 +10,10 @@ import java.util.Optional;
 
 public interface AccommodationReviewRepository extends JpaRepository<AccommodationReview, Long> {
 
-    @Query("SELECT r FROM AccommodationReview r LEFT JOIN FETCH r.comment WHERE r.accommodation.id = :accId")
+    @Query("SELECT r FROM AccommodationReview r " +
+            "LEFT JOIN FETCH r.comment " +
+            "LEFT JOIN FETCH r.guest " +
+            "WHERE r.accommodation.id = :accId")
     List<AccommodationReview> findByAccommodation_Id(@Param("accId") Long accId);
 
     // 해당 예약 ID로 작성된 리뷰 존재 여부
