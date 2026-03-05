2a6eb2a556623c8eb67654f747465a9c117f86a8
yeainlee <lyy923923@gmail.com>
2026-03-05 15:55:28 +0900

    fix: 결제가 없는 예약에서도 취소/거절이 정상 동작하도록 refund 로직 수정

--- src/main/java/com/project/cozystay/payment/service/PaymentCommandService.java
+++ src/main/java/com/project/cozystay/payment/service/PaymentCommandService.java
@@ -122,9 +122,6 @@ public class PaymentCommandService {
     // 환불 처리
     @Transactional
     public void refundByBooking(Long bookingId){
-        Payment payment = paymentRepository.findByBooking_Id(bookingId)
-                .orElseThrow(()-> new PaymentNotFoundException("예약 결제를 찾을 수 없습니다. bookingId=" + bookingId));
-
-        payment.refund();
+        paymentRepository.findByBooking_Id(bookingId).ifPresent(Payment::refund);
     }
 }
