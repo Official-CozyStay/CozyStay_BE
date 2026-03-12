809fd444d2ddb6235a88e32c431646f46f63de6c
yeainlee <lyy923923@gmail.com>
2026-03-12 14:26:01 +0900

    refactor(payment): @Data 대신 @Getter/@Setter로 DTO 명확화

--- src/main/java/com/project/cozystay/payment/dto/PaymentConfirmRequest.java
+++ src/main/java/com/project/cozystay/payment/dto/PaymentConfirmRequest.java
@@ -1,8 +1,10 @@
 package com.project.cozystay.payment.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class PaymentConfirmRequest {
     private String paymentKey;
 }
