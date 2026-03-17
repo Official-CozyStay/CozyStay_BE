275ea05e411a2571e7e13549eb020e79c36f384f
yeainlee <lyy923923@gmail.com>
2026-03-17 23:50:07 +0900

    feat: PaymentConfirmRequest에 토스 요청 구조에 맞게 orderId, amount 추가

--- src/main/java/com/project/cozystay/payment/dto/PaymentConfirmRequest.java
+++ src/main/java/com/project/cozystay/payment/dto/PaymentConfirmRequest.java
@@ -3,8 +3,12 @@ package com.project.cozystay.payment.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 public class PaymentConfirmRequest {
     private String paymentKey;
+    private String orderId;
+    private BigDecimal amount;
 }
