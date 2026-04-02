0aca260ea83b950060d57aae2836dcc9cc6febf6
Youngjae so <terranbang@gmail.com>
2026-04-02 20:52:37 +0900

    refactor : S3 업로드 기준으로 requestDTO의 URL 필드 제거

--- src/main/java/com/project/cozystay/accommodation/dto/AccommodationImageRequestDTO.java
+++ src/main/java/com/project/cozystay/accommodation/dto/AccommodationImageRequestDTO.java
@@ -10,13 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccommodationImageRequestDTO {
-    private String imageUrl;
     private Integer displayOrder;
     private Boolean isPrimary;
 
     public AccommodationImage toEntity() {
         return AccommodationImage.builder()
-                .imageUrl(this.imageUrl)
                 .displayOrder(this.displayOrder)
                 .primary(this.isPrimary != null ? this.isPrimary : false)
                 .build();
