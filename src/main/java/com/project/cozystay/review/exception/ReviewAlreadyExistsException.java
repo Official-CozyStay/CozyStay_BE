47785401352853a6f324b7540bb94009dc7ed3cd
mojiri00 <moguricamera@gmail.com>
2026-05-07 00:07:24 +0900

    refactor: ReviewAlreadyExistsException 패키지 위치 변경 (booking -> review)

---
+++ src/main/java/com/project/cozystay/review/exception/ReviewAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.project.cozystay.review.exception;
+
+public class ReviewAlreadyExistsException extends RuntimeException {
+    public ReviewAlreadyExistsException(Long bookingId) {
+        super("이미 해당 예약(ID: " + bookingId + ")에 대한 리뷰를 작성했습니다.");
+    }
+
+    public ReviewAlreadyExistsException(String message) {
+        super(message);
+    }
+}
