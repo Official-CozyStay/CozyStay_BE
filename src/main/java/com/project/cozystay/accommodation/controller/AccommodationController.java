4ec4c90e5951a4af008f57861c9dc05c0ec09c69
mojiri00 <moguricamera@gmail.com>
2026-05-12 00:41:18 +0900

    refactor: import 경로를 삭제한 @Validated 로 수정

--- src/main/java/com/project/cozystay/accommodation/controller/AccommodationController.java
+++ src/main/java/com/project/cozystay/accommodation/controller/AccommodationController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,7 +23,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/accommodations")
-@org.springframework.validation.annotation.Validated
+@Validated
 public class AccommodationController {
 
     private final AccommodationService accommodationService;
