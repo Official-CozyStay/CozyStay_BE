116407c39eaeb7583575c002ee2e82dd3397a72c
yeainlee <lyy923923@gmail.com>
2026-05-14 00:14:08 +0900

    fix: 중복된 import 삭제

--- src/main/java/com/project/cozystay/booking/guest/controller/BookingGuestMeQueryController.java
+++ src/main/java/com/project/cozystay/booking/guest/controller/BookingGuestMeQueryController.java
@@ -18,14 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
